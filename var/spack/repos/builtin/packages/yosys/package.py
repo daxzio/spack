96507b19757793b4e54acf1143ba3bdad3df3e07
Dave Keeshan <dave.keeshan@daxzio.com>
2023-10-22 22:11:37 +0100

    Redo prefix

--- var/spack/repos/builtin/packages/yosys/package.py
+++ var/spack/repos/builtin/packages/yosys/package.py
@@ -38,6 +38,7 @@ class Yosys(MakefilePackage):
         env['PREFIX'] = prefix
         env['LDFLAGS'] = f"-I${spec['ncurses'].prefix.lib}"
         env['CFLAGS'] = f"-I${spec['ncurses'].prefix.include}"
+        env['CXXFLAGS'] = f"-I${spec['ncurses'].prefix.include}"
         #env['ENABLE_READLINE'] = 0
 
 #      def build(self, spec, prefix):
