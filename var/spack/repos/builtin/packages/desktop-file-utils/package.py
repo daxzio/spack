3bfd46ff049d836ace8a33901ef62ac5df99c964
Dave Keeshan <dave.keeshan@daxzio.com>
2023-12-04 17:01:05 +0000

    Fix description

--- var/spack/repos/builtin/packages/desktop-file-utils/package.py
+++ var/spack/repos/builtin/packages/desktop-file-utils/package.py
@@ -7,15 +7,21 @@ from spack.package import *
 
 
 class DesktopFileUtils(MesonPackage):
-    """The GTK+ package contains libraries used for creating graphical user
-    interfaces for applications."""
+    """desktop-file-utils contains a few command line utilities for working with desktop entries:
+
+        * desktop-file-validate: validates a desktop file and prints warnings/errors about desktop 
+          entry specification violations.
+        * desktop-file-install: installs a desktop file to the applications directory, optionally 
+          munging it a bit in transit.
+        * update-desktop-database: updates the database containing a cache of MIME types handled 
+          by desktop files. It requires GLib to compile, because the implementation requires 
+          Unicode utilities and such."""
 
     homepage = "https://www.freedesktop.org/software/desktop-file-utils"
     url = "https://www.freedesktop.org/software/desktop-file-utils/releases/desktop-file-utils-0.27.tar.xz"
 
     version("0.27", sha256="a0817df39ce385b6621880407c56f1f298168c040c2032cedf88d5b76affe836")
 
-    # See meson.build for version requirements
     depends_on("meson", type="build")
     depends_on("ninja", type="build")
     depends_on("glib", type="build")
