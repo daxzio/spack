24a2179dbc4fb075fe3bd15e6546da6855894cea
Dave Keeshan <dave.keeshan@daxzio.com>
2023-12-07 00:30:56 +0000

    Added configurability to zlib, default is True

--- var/spack/repos/builtin/packages/opensta/package.py
+++ var/spack/repos/builtin/packages/opensta/package.py
@@ -23,26 +23,25 @@ class Opensta(CMakePackage):
 
     homepage = "https://github.com/parallaxsw/OpenSTA"
     git = "https://github.com/parallaxsw/OpenSTA.git"
-    # git = "https://github.com/The-OpenROAD-Project/OpenSTA.git"
 
     maintainers("davekeeshan")
 
     version("master", branch="master")
 
-    # version("1.1.0", sha256="f0be81afe643adc2452055e5485f09cdb509a8fdd5a4ec5547b0c31dd22b4830")
+    variant("zlib", default=True, description="build with zlib support")
     variant("cudd", default=True, description="build with cudd support")
 
     depends_on("flex", type="build")
     depends_on("tcl@8.6.11", type="build")
     depends_on("swig", type="build")
-    depends_on("zlib", type="build")
+    depends_on("zlib", type="build", when="+zlib")
     depends_on("llvm")
     depends_on("cudd", type="build", when="+cudd")
 
     def cmake_args(self):
         args = []
-        # args.append(f"-DCMAKE_CXX_FLAGS=-I{self.spec['zlib'].prefix.include}")
-        args.append(f"-DZLIB_ROOT=-{self.spec['zlib'].prefix}")
+        if self.spec.satisfies("+zlib"):
+            args.append(f"-DZLIB_ROOT=-{self.spec['zlib'].prefix}")
         if self.spec.satisfies("+cudd"):
             args.append("-DUSE_CUDD=ON ")
             args.append(f"-DCUDD_DIR={self.spec['cudd'].prefix}")
